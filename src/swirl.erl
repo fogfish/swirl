e6a40760617e59924bb6caba1d8c151a40c35f98
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2012-12-12 18:30:28 +0200

    namespace support of compiled templates

--- src/swirl.erl
+++ src/swirl.erl
@@ -21,7 +21,19 @@
 %%
 %% swirl -o Dir INPUT
 main(["-o", Dir, Input]) ->
+   % compile template into beam file
    cc(Input, [{outdir, Dir}]);
+
+main(["-j", Json, Input]) ->
+   % bind a template with json
+   {ok, S} = file:read_file(Input),
+   {ok, J} = file:read_file(Json),
+   R = r(
+      binary_to_list(S), 
+      jsx:decode(J, [{labels, atom}])
+   ),
+   io:format("~s", [R]);
+
 main([]) ->
    io:format("Usage: swirl -o Dir INPUT\n").
 
@@ -56,25 +68,39 @@ cc(File) ->
 
 cc(File, Opts) ->
    {ok, Bstr} = file:read_file(File),
-   Uid = list_to_atom(
-      filename:basename(File, filename:extension(File))
-   ),
+   Uid = list_to_atom(uid(File, Opts)),
    {ok, Mod, Bin} = cc_mod(Uid, binary_to_list(Bstr)),
    {module, Uid}  = code:load_binary(Mod, [], Bin),
-   case proplists:get_value(outdir, Opts) of
-      undefined ->
-         ok;
-      Dir       ->
-         Beam = filename:join([Dir, filename:basename(File, filename:extension(File)) ++ ".beam"]),
-         file:write_file(Beam, Bin)
+   case beam(File, Opts) of
+      undefined -> ok;
+      Beam      -> file:write_file(Beam, Bin)
    end.
-
 
 %%%----------------------------------------------------------------------------   
 %%%
 %%% private
 %%%
 %%%----------------------------------------------------------------------------   
+
+%%
+%%
+uid(File, Opts) ->
+   case proplists:get_value(namespace, Opts) of
+      % name space is not defined, uid is filename
+      undefined -> 
+         filename:basename(File, filename:extension(File));
+      Ns        -> 
+         Ns ++ "_" ++ filename:basename(File, filename:extension(File))
+   end.
+
+%%
+%%
+beam(File, Opts) ->
+   case proplists:get_value(outdir, Opts) of
+      undefined -> undefined;
+      Dir       -> filename:join([Dir, uid(File, Opts) ++ ".beam"])
+   end.
+
 
 %%
 %% cc_mod(Uid, Str) -> {ok, Mod, Bin}
